40745647e347405123c9566feb1b671101b4d65d
young-rae-kim <dufo7070@kaist.ac.kr>
2022-07-26 18:50:49 +0900

    Add kakao login

--- 0.1.0/src/views/LoginPage.vue
+++ 0.1.0/src/views/LoginPage.vue
@@ -43,7 +43,7 @@ export default {
         return {
             redirect: (this.$route.query.redirect)
                 ? this.$route.query.redirect
-                : '/mypage'
+                : '/'
         }
     },
     methods: {
