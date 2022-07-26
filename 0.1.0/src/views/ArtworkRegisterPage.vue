d168910e15fa1e5199b98ee3cd32b12731a73571
Nuetee <sungjoon729@gmail.com>
2022-07-26 18:50:53 +0900

    artworkRegisterPage 수정중

--- 0.1.0/src/views/ArtworkRegisterPage.vue
+++ 0.1.0/src/views/ArtworkRegisterPage.vue
@@ -1,15 +1,45 @@
 <template>
     <div id="artworkRegisterPage">
-
+        <div class="top">
+            <div class="cancelButton">취소</div>
+            <div>작품명 입력</div>
+            <div class="nextButton">다음</div>
+        </div>
+        <div class="bottom">
+            <swiper v-bind="this.swiperOptions">
+                <swiper-slide>Slide 1</swiper-slide>
+                <swiper-slide>Slide 2</swiper-slide>
+                <swiper-slide>Slide 3</swiper-slide>
+                <swiper-slide>Slide 4</swiper-slide>
+            </swiper>
+        </div>
     </div>
 </template>
 <script>
+    import SwiperCore, { Pagination } from 'swiper';
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+    import 'swiper/css';
+    import "swiper/css/pagination";
+
+    SwiperCore.use([Pagination]);
+
     export default {
         name: 'ArtworkRegisterPage',
-        components: {},
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
         data() {
             return {
-                sampleData: ''
+                swiperOptions: {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                    loop: false,
+                    centeredSlides: true,
+                    pagination: {
+                        type: 'progressbar',
+                    }
+                },
             };
         },
         beforeCreate() {},
@@ -23,3 +53,4 @@
         methods: {}
     }
 </script>
+<style lang="scss" scoped src="../scss/ArtworkRegisterPage/artworkRegisterPage.scss"></style>
