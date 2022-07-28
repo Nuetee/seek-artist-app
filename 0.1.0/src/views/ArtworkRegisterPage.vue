81312dc3169b1fbd035d2591d68baf1789c1038f
Nuetee <sungjoon729@gmail.com>
2022-07-28 15:58:09 +0900

    ArtworkRegisterPage 수정.

--- 0.1.0/src/views/ArtworkRegisterPage.vue
+++ 0.1.0/src/views/ArtworkRegisterPage.vue
@@ -1,18 +1,31 @@
 <template>
     <div id="artworkRegisterPage">
         <div class="top">
-            <div class="previousButton">취소</div>
-            <div>작품명 입력</div>
-            <div class="nextButton">다음</div>
+            <div class="prevButton" @click="this.swiperNavigation(0);">
+                <div v-if="this.swiperIndex === 0">취소</div>
+                <svg v-else width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 13L1 7L7 1" stroke="black" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+            </div>
+            <div class="presentStep"> {{ this.presentStep[this.swiperIndex] }} </div>
+            <div class="nextButton" @click="this.swiperNavigation(1)" :style="'color: ' + this.fontColor">
+                <div v-if="this.swiperIndex === 3">완료</div>
+                <div v-else>다음</div>
+            </div>
         </div>
         <div class="bottom">
-            <swiper v-bind="this.swiperOptions">
-                <swiper-slide>Slide 1</swiper-slide>
-                <swiper-slide>Slide 2</swiper-slide>
+            <swiper v-bind="this.swiperOptions" @slideChange="this.slideChange">
+                <swiper-slide>
+                    <TitleInput @activate-next-button="this.activateNextButton"></TitleInput>
+                </swiper-slide>
+                <swiper-slide>
+                    <ImageSelection></ImageSelection>
+                </swiper-slide>
                 <swiper-slide>Slide 3</swiper-slide>
                 <swiper-slide>Slide 4</swiper-slide>
-                <div class="nextButton"></div>
-                <div class="prevButton"></div>
+                <button class="next"></button>
+                <button class="previous"></button>
             </swiper>
         </div>
     </div>
@@ -22,6 +35,8 @@
     import { Swiper, SwiperSlide } from 'swiper/vue';
     import 'swiper/css';
     import "swiper/css/pagination";
+    import TitleInput from '@/components/ArtworkRegisterPage/TitleInput.vue';
+    import ImageSelection from '@/components/ArtworkRegisterPage/ImageSelection.vue';
 
     SwiperCore.use([Pagination, Navigation]);
 
@@ -30,9 +45,15 @@
         components: {
             Swiper,
             SwiperSlide,
+            TitleInput,
+            ImageSelection
         },
         data() {
             return {
+                presentStep: ['작품명 입력', '이미지 선택', '상세정보 입력', '작품 설명 입력'],
+                swiperIndex: 0,
+                navigationButtons: [],
+                fontColor: '#959595;',
                 swiperOptions: {
                     slidesPerView: 1,
                     spaceBetween: 0,
@@ -43,8 +64,8 @@
                         type: 'progressbar',
                     },
                     navigation: {
-                        nextEl: '.nextButton',
-                        prevEl: '.prevButton'
+                        nextEl: '.next',
+                        prevEl: '.previous'
                     }
                 },
             };
@@ -52,12 +73,50 @@
         beforeCreate() {},
         created() {},
         beforeMount() {},
-        mounted() {},
+        mounted() {
+            this.navigationButtons.push(document.getElementsByClassName('previous')[0])
+            this.navigationButtons.push(document.getElementsByClassName('next')[0])
+            
+            this.navigationButtons[1].disabled = true
+        },
         beforeUpdate() {},
         updated() {},
         beforeUnmount() {},
         unmounted() {},
-        methods: {}
+        methods: {
+            /*
+            * - "다음" 또는 "<(이전)" 버튼을 누르면 활성화 되는 함수
+            * - buttonIndex를 parameter로 받는다. "<"버튼은 buttonIndex == 0, "다음" 버튼은 buttonIndex == 1.
+            * 1. "<" 또는 "다음" 버튼을 눌렀을 때, 실제 swiperNavigation이 binding 돼 있는 버튼의 click 이벤트를 발생시킨다.
+            * : 실제 navigation을 관장하는 button은 배열 navigationButtons에 담겨있다.
+            * 2. "다음" 버튼을 누른 경우(buttonIndex == 1) "다음" 버튼을 비활성화 한다.
+            * 3. "<" 버튼을 누른 경우(buttonIndex == 0) "다음" 버튼을 활성화 한다.
+            */
+            swiperNavigation (buttonIndex) {
+                this.navigationButtons[buttonIndex].click()
+                if (buttonIndex) {
+                    this.navigationButtons[buttonIndex].disabled = true
+                    this.fontColor = '#959595'
+                }
+                else {
+                    this.navigationButtons[buttonIndex].disabled = false
+                    this.fontColor = '#000000'
+                }
+            },
+            slideChange (swiper) {
+                this.swiperIndex = swiper.activeIndex
+            },
+            activateNextButton (isActive) {
+                this.navigationButtons[1].disabled = !isActive
+
+                if (isActive) {
+                    this.fontColor = '#000000'
+                }
+                else {
+                    this.fontColor = '#959595'
+                }
+            }
+        }
     }
 </script>
 <style lang="scss" scoped src="../scss/ArtworkRegisterPage/artworkRegisterPage.scss"></style>
---
+++ 0.1.0/src/widgets/ArtworkInfo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="artworkInfo poppins">
+        <div :style="(this.isBack) ? { color: 'black' } : { color: this.color }" class="artworkTitle">
+            {{ this.artwork.getName() }}
+        </div>
+        <div :style="(this.isBack) ? { color: 'black' } : { color: this.color }" class="artist">
+            {{ this.artwork.getArtist().getNickname() }}
+            <SNSLink v-if="this.sns" :artwork="this.artwork"></SNSLink>
+        </div>
+    </div>
+</template>
+<script>
+import SNSLink from '@/components/ArtworkModifyPage/SNSLink.vue'
+
+export default {
+    name: 'ArtworkInfo',
+    components: { SNSLink },
+    props: {
+        artwork: Object,
+        isBack: Boolean
+    },
+    data() {
+        return {
+            color: this.artwork.getColor(),
+            sns: this.artwork.getArtist().getSNS()
+        }
+    }
+}
+</script>
+<style lang="scss" scoped src="../scss/widgets/artworkInfo.scss">
+</style>
