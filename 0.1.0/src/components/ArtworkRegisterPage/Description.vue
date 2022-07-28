b1cf11d109a036a0ee5cfeb9c6c0d1dd54dfd851
Nuetee <sungjoon729@gmail.com>
2022-07-28 18:54:15 +0900

    ArtworkRegisterPage slide 1 ~ 3 잠정 완료.

---
+++ 0.1.0/src/components/ArtworkRegisterPage/Description.vue
@@ -0,0 +1,87 @@
+<template>
+    <div id="description">
+        <div class="material">
+            <div class="label">재료</div>
+            <div class="background">
+                <textarea class="input" v-model="this.material" name="material" cols="30" rows="1"
+                    placeholder="클레이"></textarea>
+            </div>
+        </div>
+        <div class="size">
+            <div class="label">크기</div>
+            <div class="form poppins">
+                <div class="background">
+                    <input class="input" placeholder="20" type="number" v-model="this.size.x" />
+                </div>
+                x
+                <div class="background">
+                    <input class="input" placeholder="20" type="number" v-model="this.size.y" />
+                </div>
+                x
+                <div class="background">
+                    <input class="input" placeholder="20" type="number" v-model="this.size.z" />
+                </div>
+                <div class="background unit">
+                    <select class="input" name="unit" id="unit-select" v-model="this.unit">
+                        <option value="mm">mm</option>
+                        <option value="cm">cm</option>
+                        <option value="m">m</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="manufacturedDate">
+            <div class="label">제작연도</div>
+            <div class="background poppins">
+                <input class="input" placeholder="2022" type="number" v-model="this.year" />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Description',
+        components: {},
+        data() {
+            return {
+                material: '',
+                size: {
+                    x: null,
+                    y: null,
+                    z: null,
+                },
+                unit: 'cm',
+                year: null
+            };
+        },
+        watch: {
+            'material': 'validCheck',
+            size: {
+                deep: true,
+                handler() {
+                    this.validCheck()
+                }
+            },
+            'year': 'validCheck'
+        },
+        beforeCreate() {},
+        created() {},
+        beforeMount() {},
+        mounted() {},
+        beforeUpdate() {},
+        updated() {},
+        beforeUnmount() {},
+        unmounted() {},
+        methods: {
+            validCheck () {
+                if (this.material && this.size.x && this.size.y && this.year) {
+                    this.$emit('activate-next-button', true)
+                }
+                else {
+                    this.$emit('activate-next-button', false)
+                }
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped src="../../scss/ArtworkRegisterPage/description.scss"></style>
--- 0.1.0/src/components/ArtworkRegisterPage/ImageSelection.vue
+++ 0.1.0/src/components/ArtworkRegisterPage/ImageSelection.vue
@@ -1,12 +1,19 @@
 <template>
     <div id="imageSelection">
         <div>권장 비율 3 : 5</div>
+        <input type="file" id="imageUpload" accept="image/*" multiple>
         <swiper v-bind="this.swiperOptions">
-            <swiper-slide id="imageAddition">+</swiper-slide>
+            <swiper-slide v-for="(image, i) in this.selectedImages">
+                <img :src="image.src" :style="image.style">
+            </swiper-slide>
+            <swiper-slide id="imageAddition">
+                <label for="imageUpload"> + </label>
+            </swiper-slide>
         </swiper>
     </div>
 </template>
 <script>
+    import { cropImage } from '@/modules/image';
     import { Swiper, SwiperSlide } from 'swiper/vue';
     import 'swiper/css';
 
@@ -18,6 +25,7 @@
         },
         data() {
             return {
+                imageUpload: null,
                 selectedImages: [],
                 swiperOptions: {
                     slidesPerView: 1,
@@ -28,15 +36,63 @@
                 },
             };
         },
+        watch: {
+            selectedImages: {
+                deep: true,
+                async handler(newVal) {
+                    await this.$nextTick()
+                    if (newVal.length > 0) {
+                        this.$emit('activate-next-button', true)
+                    }
+                    else {
+                        this.$emit('activate-next-button', false)
+                    }
+                }
+            },
+        },
         beforeCreate() {},
         created() {},
         beforeMount() {},
-        mounted() {},
+        mounted() {
+            this.imageUpload = document.getElementById('imageUpload')
+            imageUpload.addEventListener('change', this.addImageSlide)
+        },
         beforeUpdate() {},
         updated() {},
         beforeUnmount() {},
         unmounted() {},
-        methods: {}
+        methods: {
+            async addImageSlide () {
+                const selectedfiles = this.imageUpload.files
+                let fileLength = selectedfiles.length
+
+                for (let i = 0; i < fileLength; i++) {
+                    let image = new Object()
+                    image.name = selectedfiles[i].name
+                    image.src = URL.createObjectURL(selectedfiles[i])
+                    image.style = await cropImage(image.src, 3/5)
+
+                    if (this.checkImage(image.name)) {
+                        this.selectedImages.push(image)
+                    }
+                }
+                imageUpload.value = ''
+            },
+            checkImage (imageName) {
+                const length = this.selectedImages.length
+                if (length >= 5) {
+                    alert('이미지는 최대 5장까지 첨부 가능합니다.')
+                    return false
+                }
+                for (let i = 0; i < length; i++) {
+                    if (this.selectedImages[i].name === imageName) {
+                        alert('중복된 이미지는 업로드 할 수 없습니다.')
+                        return false
+                    }
+                }
+                return true
+            }
+        }
     }
 </script>
 <style lang="scss" scoped src="../../scss/ArtworkRegisterPage/imageSelection.scss"></style>
