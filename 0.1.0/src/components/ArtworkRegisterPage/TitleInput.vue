979f037279bab2ead0d59eb6d865a6576b0fa4a4
Nuetee <sungjoon729@gmail.com>
2022-07-28 15:58:09 +0900

    ArtworkRegisterPage 수정.

---
+++ 0.1.0/src/components/ArtworkRegisterPage/TitleInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="titleInput">
+        <div>
+            <textarea v-model="this.title" name="title" id="title" cols="30" rows="1" placeholder="작품명을 입력하세요"></textarea>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'TitleInput',
+        components: {},
+        data() {
+            return {
+                title: ''
+            };
+        },
+        watch: {
+            'title': {
+                async handler (newVal, oldVal) {
+                    await this.$nextTick()
+                    if (newVal.length > 0) {
+                        this.$emit('activate-next-button', true)
+                    }
+                    else {
+                        this.$emit('activate-next-button', false)
+                    }
+                },
+                immediate: true
+            }
+        },
+        beforeCreate() {},
+        created() {},
+        beforeMount() {},
+        mounted() {},
+        beforeUpdate() {},
+        updated() {},
+        beforeUnmount() {},
+        unmounted() {},
+        methods: {}
+    }
+</script>
+<style lang="scss" scoped src="../../scss/ArtworkRegisterPage/titleInput.scss"></style>
---
+++ 0.1.0/src/components/MyPage/UploadButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="uploadButton">
+        Upload
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 19V5" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M5 12L12 5L19 12" stroke="white" stroke-width="2.5" stroke-linecap="round"
+                stroke-linejoin="round" />
+        </svg>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'UploadButton',
+        components: {},
+        data() {
+            return {
+                sampleData: ''
+            };
+        },
+        beforeCreate() {},
+        created() {},
+        beforeMount() {},
+        mounted() {},
+        beforeUpdate() {},
+        updated() {},
+        beforeUnmount() {},
+        unmounted() {},
+        methods: {}
+    }
+</script>
+<style lang="scss" scoped src="../../scss/MyPage/uploadButton.scss"></style>
