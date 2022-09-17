b9810ef111fe84c3c1c43d2a083ea89ca47bb892
young-rae-kim <dufo7070@kaist.ac.kr>
2022-09-17 16:26:15 +0900

    Add test page

---
+++ 0.1.0/src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="file" id="videoUpload" accept="video/mp4">
+    <button @click="this.submit" type="submit"></button>
+  </div>
+</template>
+
+<script>
+import { transcode } from '@/modules/image';
+
+export default {
+    methods: {
+        submit () {
+            const video = document.getElementById('videoUpload')
+            console.log(video)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
