f4cdb83ea312ac1021537eb0557f999aca5cac4d
young-rae-kim <dufo7070@kaist.ac.kr>
2022-09-24 00:03:23 +0900

    Delete test code

--- 0.1.0/src/views/MyPage.vue
+++ 0.1.0/src/views/MyPage.vue
@@ -65,7 +65,6 @@
     import SideBar from '@/widgets/SideBar.vue';
     import UploadButton from '@/components/MyPage/UploadButton.vue';
     import Background from '../widgets/Background.vue';
-    import { Exhibition } from '@/classes/exhibition';
 
     import { Swiper, SwiperSlide } from 'swiper/vue';
     import SwiperCore, { Navigation } from 'swiper';
@@ -124,10 +123,6 @@
                     return
                 }
             }
-
-            const ex = await new Exhibition('1').init()
-            await ex.initializePage()
-            console.log(await ex.getCollaboratorList())
 
             //Update session
             if (isAuth()) {
