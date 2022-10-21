7390422b7eb74c2b576f42761f86e781fabf3f79
Nuetee <sungjoon729@gmail.com>
2022-10-21 22:45:04 +0900

    내전시/협업전시 표사

--- 0.1.0/src/modules/image.js
+++ 0.1.0/src/modules/image.js
@@ -19,7 +19,8 @@ function dataURItoBlob (dataURI) {
 // Get css string for cropped image
 // - image_source : source of image (after url has been mapped)
 // return css style for cropped image, empty string if error occurred
- // ratio = width/height
+// ratio = width/height
+// if fill_container == true, return style that makes image fill container, else, return style that makes the entire image visible
 export async function cropImage (image_source, container_ratio, fill_container = true) {
     const crop_process = (src, ratio, fill) => {
         return new Promise(function (resolve) {
