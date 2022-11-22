7422506fc0e0ab4b9e834b04f8622af9185a8108
Nuetee <sungjoon729@gmail.com>
2022-11-22 18:38:20 +0900

    tag list

--- 0.1.0/src/App.vue
+++ 0.1.0/src/App.vue
@@ -51,8 +51,8 @@
   }
 </script>
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
   :root {
     --vw: 1vw;
     --vh: 1vh;
